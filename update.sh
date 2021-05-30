631cc491e8599349a7e3fc97f2ee326c9e32b90c
nazo6 <53177992+nazo6@users.noreply.github.com>
2021-05-30 11:26:00 +0900

    Updated sources.

---
+++ update.sh
@@ -0,0 +1,7 @@
+cd sources
+wget -O api.txt https://raw.githubusercontent.com/neovim/neovim/master/runtime/doc/api.txt
+wget -O eval.txt https://raw.githubusercontent.com/neovim/neovim/master/runtime/doc/eval.txt
+wget -O testing.txt https://raw.githubusercontent.com/neovim/neovim/master/runtime/doc/testing.txt
+wget -O sign.txt https://raw.githubusercontent.com/neovim/neovim/master/runtime/doc/sign.txt
+wget -O lua.txt https://raw.githubusercontent.com/neovim/neovim/master/runtime/doc/lua.txt
+wget -O lsp.txt https://raw.githubusercontent.com/neovim/neovim/master/runtime/doc/lsp.txt
